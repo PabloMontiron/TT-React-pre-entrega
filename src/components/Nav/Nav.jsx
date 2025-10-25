f2268e7a541e684576c517b94c2f0d4f9a4f12a4
Pablo Montiron <pmontiron@gmail.com>
2025-10-24 22:27:57 -0300

    Correccion nombre de rutas

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { useCartContext } from "../../context/cartContext/useCartContext";
+import { useCartContext } from "../../context/CartContext/useCartContext";
 import "./nav.css"
 
 export const Nav = () => {
